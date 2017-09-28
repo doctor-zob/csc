a08e418dc9b2f5b37b783504d567a9a10ff7a28a
doctor-zob <doctor.zob@gmail.com>
2017-09-28 09:54:40 +0100

    update variables

--- config/site.php
+++ config/site.php
@@ -1,7 +1,7 @@
 <?php
 
 define('DB_SERVER', getenv("MYSQL_SERVICE_HOST").":".getenv("MYSQL_SERVICE_PORT"));
-define('DB_USERNAME', 'adminiiaQYsn');
-define('DB_PASSWORD', 'le-nZPZPYQeh');
+define('DB_USERNAME', '$MYSQL_DB_USERNAME');
+define('DB_PASSWORD', '$MYSQL_DB_PASSWORD');
 define('DB_DATABASE', 'club');
 define('ENABLE_NEWSFLOW_OVERLAY', false);
